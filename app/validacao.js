77f6e441ee2912870191bd31ec36f6ba9b06b7d3
Gabriel Mendes <luizgabrielpm@gmail.com>
2022-12-23 12:57:19 -0300

    Tela de Game Over
    Quando o jogador falar 'Game Over', o jogo é encerrado e o usuário é
    levado para uma tela escrita Game Over, revelando o número secreto e
    exibindo um botão para ele jogar novamente.

--- app/validacao.js
+++ app/validacao.js
@@ -1,32 +1,45 @@
 function chuteValido(chute) {
     const numero = +chute;
 
-    if(Number.isNaN(numero)) {
-        elementoChute.innerHTML += '<div>Valor inválido</div>';
-        return;
-    }
-
-    if(numero > maiorValor || numero < menorValor) {
-        elementoChute.innerHTML += `<div>Valor inválido: o número secreto precisa estar entre ${menorValor} e ${maiorValor}.</div>`;
-        return;
-    }
-
-    if(numero === numeroSecreto) {
+    // Gave Over
+    console.log(chute);
+    if(chute == "Game Over."){
         document.body.innerHTML = `
-            <h2>Você acertou!</h2>
-            <h3>O número secreto era ${numeroSecreto}.</h3>
+        <h1>Game Over!</h1>
+        <h3>O número secreto era ${numeroSecreto}.</h3>
 
-            <button id="jogar-novamente" class="btn-jogar">Jogar Novamente</button>
-        `; 
-    } else if(numero > numeroSecreto){
-        elementoChute.innerHTML += `
-        <div>O número secreto é menor <i class="fa-solid fa-arrow-down"></i></div>
-        `
+        <button id="jogar-novamente" class="btn-jogar">Jogar Novamente</button>
+        `;
     } else {
-        elementoChute.innerHTML += `
-        <div>O número secreto é maior <i class="fa-solid fa-arrow-up"></i></div>
-        ` 
+        if(Number.isNaN(numero)) {
+            elementoChute.innerHTML += '<div>Valor inválido</div>';
+            return;
+        }
+    
+        if(numero > maiorValor || numero < menorValor) {
+            elementoChute.innerHTML += `<div>Valor inválido: o número secreto precisa estar entre ${menorValor} e ${maiorValor}.</div>`;
+            return;
+        }
+    
+        if(numero === numeroSecreto) {
+            document.body.innerHTML = `
+                <h1>Você acertou!</h1>
+                <h3>O número secreto era ${numeroSecreto}.</h3>
+    
+                <button id="jogar-novamente" class="btn-jogar">Jogar Novamente</button>
+            `; 
+        } else if(numero > numeroSecreto){
+            elementoChute.innerHTML += `
+            <div>O número secreto é menor <i class="fa-solid fa-arrow-down"></i></div>
+            `
+        } else {
+            elementoChute.innerHTML += `
+            <div>O número secreto é maior <i class="fa-solid fa-arrow-up"></i></div>
+            ` 
+        }
     }
+
+    
 }
 
 document.body.addEventListener("click", evento => {
